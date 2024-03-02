e589f5957cc98fd196033501b5c72a1a6cb3ff20
neffarrty <egorkovtun8@gmail.com>
2024-03-02 20:14:29 +0200

    remove unnecessary struct & includes from client/inc/uchat.h

--- client/inc/uchat.h
+++ client/inc/uchat.h
@@ -2,14 +2,6 @@
 #define UCHAT_H
 
 #include <gtk/gtk.h>
-#include <regex.h>
-#include <ctype.h>
-
-typedef struct sign_in_form {
-    GtkWindow *window;
-    GtkEntry  *username;
-    GtkEntry  *password;
-}   sign_in_form;
 
 #endif
 
