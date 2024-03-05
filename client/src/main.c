0d2e1ccbc12b5597aaa00d212012ddba04232325
neffarrty <egorkovtun8@gmail.com>
2024-03-05 12:11:02 +0200

    add test_server.c

--- client/src/main.c
+++ client/src/main.c
@@ -4,6 +4,34 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+
+int connect_to_server(const char* ip, int port) {
+    int sockfd = 0;
+    struct sockaddr_in server_addr;
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr = (struct sockaddr_in) { 
+        .sin_family = AF_INET,
+        .sin_port = htons(port),
+        .sin_addr.s_addr = inet_addr(ip)
+    };
+
+    if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) < 0) {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+
+    return sockfd;
+}
+
+int serv_sock;
 
 static void connect_css(const char* filename) {
     GtkCssProvider *provider = gtk_css_provider_new();
@@ -33,38 +61,17 @@ void entry_get_formated_text(char* buf, const char* format, GtkEntry* entry) {
 }
 
 void login_button_click_cb(GtkWidget *button, gpointer data) {
-    GtkWidget *dialog, *username_label, *password_label, *content_area;
-    char username_buf[1024] = { 0 };
-    char password_buf[1024] = { 0 };
-    GtkBuilder *builder = GTK_BUILDER(data);
+    if(button != NULL) {
+        char username_buf[1024] = { 0 };
+        char password_buf[1024] = { 0 };
+        GtkBuilder *builder = GTK_BUILDER(data);
 
-    gtk_button_set_label(GTK_BUTTON(button), "Sign in");
+        entry_get_formated_text(username_buf, "USERNAME: %s", GTK_ENTRY(gtk_builder_get_object(builder, "username-entry")));
+        entry_get_formated_text(password_buf, "PASSWORD: %s", GTK_ENTRY(gtk_builder_get_object(builder, "password-entry")));
 
-    dialog = gtk_dialog_new_with_buttons("Login", GTK_WINDOW(gtk_builder_get_object(builder, "window")), GTK_DIALOG_DESTROY_WITH_PARENT, "OK", GTK_RESPONSE_OK, NULL);
-    gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
-    gtk_window_set_default_size(GTK_WINDOW(dialog), 300, 200);
-
-    content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
-    gtk_widget_set_valign(content_area, GTK_ALIGN_CENTER);
-    gtk_widget_set_halign(content_area, GTK_ALIGN_CENTER);
-
-    entry_get_formated_text(username_buf, "USERNAME: %s", GTK_ENTRY(gtk_builder_get_object(builder, "username-entry")));
-    entry_get_formated_text(password_buf, "PASSWORD: %s", GTK_ENTRY(gtk_builder_get_object(builder, "password-entry")));
-
-    username_label = gtk_label_new(username_buf);
-    gtk_widget_set_valign(username_label, GTK_ALIGN_CENTER);
-    gtk_widget_set_halign(username_label, GTK_ALIGN_START);
-
-    gtk_box_append(GTK_BOX(content_area), username_label);
-
-    password_label = gtk_label_new(password_buf);
-    gtk_widget_set_valign(password_label, GTK_ALIGN_CENTER);
-    gtk_widget_set_halign(password_label, GTK_ALIGN_START);
-
-    gtk_box_append(GTK_BOX(content_area), password_label);
-
-    gtk_window_set_child(GTK_WINDOW(dialog), content_area);
-    gtk_window_present(GTK_WINDOW(dialog));
+        send(serv_sock, username_buf, sizeof username_buf, 0);
+        send(serv_sock, password_buf, sizeof password_buf, 0);
+    }
 }
 
 void app_activate_cb(GtkApplication *app) {
@@ -89,36 +96,16 @@ void app_activate_cb(GtkApplication *app) {
     }
 }
 
-void connect_to_server(const char* ip, int port) {
-    int sockfd = 0;
-    struct sockaddr_in server_addr;
-
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-    server_addr.sin_port = htons(port);
-
-    if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) < 0) {
-        perror("connect");
-        exit(EXIT_FAILURE);
-    }
-
-    send(sockfd, "pisyun", sizeof("pisyun"), 0);
-}
-
 int main(int argc, char *argv[]) {
     if(argc != 3) {
-        fprintf(stderr, "usage: ./uchat <ip-addr> <port>\n");
+        fprintf(stderr, "usage: uchat <server-ip> <port>\n");
         return EXIT_FAILURE;
     }
-    connect_to_server(argv[1], atoi(argv[2]));
-    
+
     GtkApplication *app = NULL;
     int status = 0;
+
+    serv_sock = connect_to_server(argv[1], atoi(argv[2]));
 
     app = gtk_application_new("ua.ucode-connect.uchat", G_APPLICATION_HANDLES_OPEN);
 
