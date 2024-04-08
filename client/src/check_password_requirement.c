7ce44360debb7374d53863ba01255d03403aa1ae
neffarrty <egorkovtun8@gmail.com>
2024-04-08 22:19:26 +0300

    update home page

--- client/src/check_password_requirement.c
+++ client/src/check_password_requirement.c
@@ -4,12 +4,12 @@ void check_password_requirement(bool req, GtkBuilder* builder, int id) {
     GtkWidget* label = GTK_WIDGET(gtk_builder_get_object(builder, req_fields[id][BUILDER_ID]));
     
     if(!req) {
-        gtk_widget_add_css_class(label, "wrong-password");
+        gtk_widget_add_css_class(label, "error-label");
         gtk_label_set_label(GTK_LABEL(label), req_fields[id][WRONG_LABEL]);
     }
     else {
-        if(widget_has_css_class(label, "wrong-password")) {
-            gtk_widget_remove_css_class(label, "wrong-password");
+        if(widget_has_css_class(label, "error-label")) {
+            gtk_widget_remove_css_class(label, "error-label");
         }
         gtk_label_set_label(GTK_LABEL(label), req_fields[id][CORRECT_LABEL]);
     }
