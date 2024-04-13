5758b0ad90b8b15184527ac456209a7db890656a
t1tandr <t1tandr@Ubuntu.myguest.virtualbox.org>
2024-04-13 12:05:00 +0300

    gtk critical with window is fixed

--- client/src/callbacks/register_button_click_cb.c
+++ client/src/callbacks/register_button_click_cb.c
@@ -6,9 +6,9 @@ void register_button_click_cb(GtkButton* self, gpointer user_data) {
 
     gtk_revealer_set_reveal_child(revealer, TRUE);
 
-    GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "main-window"));
     GtkWidget* homepage = GTK_WIDGET(gtk_builder_get_object(builder, "homepage"));
     GtkWidget* login_page = GTK_WIDGET(gtk_builder_get_object(builder, "login-page"));
+    GtkWindow* window = GTK_WINDOW(gtk_widget_get_parent(login_page));
 
     gtk_widget_unparent(login_page);
     gtk_window_set_child(window, homepage);
