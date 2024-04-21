caa703bc1efda5851a008c349f9a0f9d8c0f2001
azhupanov <azhupanov@stud.khpi.ucode-connect.study>
2024-04-21 20:23:16 +0200

    add option to change name and bio

--- client/src/get_user_from_json.c
+++ client/src/get_user_from_json.c
@@ -4,21 +4,17 @@ t_user* get_user_from_json(cJSON* json) {
     if(json == NULL) {
         return NULL;
     }
-    cJSON* data = cJSON_GetObjectItemCaseSensitive(json, "data");
-    if (data == NULL) {
-        cJSON_Delete(json);
-        return NULL;
-    }
 
     t_user* user = (t_user *)malloc(sizeof(t_user));
-    user->id = cJSON_GetObjectItemCaseSensitive(data, "user_id")->valueint;
-    user->session = strdup(cJSON_GetObjectItemCaseSensitive(data, "session_id")->valuestring);
+    user->id = cJSON_GetObjectItemCaseSensitive(json, "user_id")->valueint;
+    user->session = strdup(cJSON_GetObjectItemCaseSensitive(json, "session_id")->valuestring);
+
     /*
     user->username = strdup(cJSON_GetObjectItemCaseSensitive(data, "username")->valuestring);
     user->name = strdup(cJSON_GetObjectItemCaseSensitive(data, "name")->valuestring);
     user->bio = strdup(cJSON_GetObjectItemCaseSensitive(data, "bio")->valuestring);
-
     */
+   
     return user;
 }
 
