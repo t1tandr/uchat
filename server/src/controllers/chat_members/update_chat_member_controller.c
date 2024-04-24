f510512045990eb710d940c070f0e88ff83a57b6
Prov258 <prov.offcl@gmail.com>
2024-04-24 09:32:15 +0300

    fix response chat member

--- server/src/controllers/chat_members/update_chat_member_controller.c
+++ server/src/controllers/chat_members/update_chat_member_controller.c
@@ -10,6 +10,19 @@ void update_chat_member_controller(int chat_id, cJSON *req, sqlite3 *db, int soc
 
     if (!chat_member) return;
 
-    send_response(sock_fd, chat_member, 201);
+    cJSON *chat_members = get_chat_members_service(
+        cJSON_GetObjectItem(chat_member, "chat_id")->valueint,
+        headers,
+        db,
+        sock_fd
+    );
+    
+    if (!chat_members) return;
+
+    GHashTable *user_ids = chat_members_to_user_ids_set(chat_members);
+
+    cJSON_Delete(chat_members);
+
+    send_response_users_by_id(user_ids, chat_member, 201, sock_fd);
 }
 
